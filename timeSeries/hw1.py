335594b38cb1d585207c901eb4aa419223ecfc90
Michael Delcid <michaeldelcid16@gmail.com>
2021-02-15 22:31:32 -0500

    modified hw1.py

--- timeSeries/hw1.py
+++ timeSeries/hw1.py
@@ -9,8 +9,27 @@ df = pd.read_csv(fileName)
 #Second parameter searches all columns starting with 'Mkt-RF (Market-risk free)' ends with 'Momentum'
 
 correlation = df.corr().loc['Food':'Other', 'Mkt-RF (Market-risk free)':'Momentum']
-print(correlation)
 
+#Save five factor returns
+five_factors = df.loc[:,'Mkt-RF (Market-risk free)':'Momentum'] 
+expected_value = five_factors.mean()
+
+#Create time series for five factors, then compute auto correlation with lag 
+
+MKTRF_Series = df.loc[:,'Mkt-RF (Market-risk free)']
+SMB_Series = df.loc[:,'SMB (Small minus big)']
+HML_Series = df.loc[:,'HML (High minus low)']
+RF_Series = df.loc[:,'RF (Risk free rate)']
+Momentum_Series = df.loc[:,'Momentum']
+
+autocorr = pd.DataFrame({'Mkt-RF (Market-risk free)': [], 'SMB (Small minus big)': [], 'HML (High minus low)': [],'RF (Risk free rate)': [], 'Momentum': []})
+
+for i in range(11):
+     autocorr = autocorr.append({'Mkt-RF (Market-risk free)': MKTRF_Series.autocorr(lag=i), 'SMB (Small minus big)': SMB_Series.autocorr(lag=i), 'HML (High minus low)': HML_Series.autocorr(lag=i),'RF (Risk free rate)': RF_Series.autocorr(lag=i), 'Momentum': Momentum_Series.autocorr(lag=i)}, ignore_index = True)
+
+
+print(correlation)
 print('\n1. Mkt-RF correlates most highly with every industry')
 print('2. Momentum correlates negatively with each industry')
-print('3. RF does not correlate highly with the 30 industry time series. In fact it is weakly correlated')
+print('3. RF does not correlate highly with the 30 industry time series. In fact it is weakly correlated\n')
+print(autocorr)
